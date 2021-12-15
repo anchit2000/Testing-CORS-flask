fcdd05f90c7259bdbe9164eb4a28337f8df32ca5
Anchit Shrivastava <anchit.s@tatrasdata.com>
2021-12-15 13:35:59 +0530

    added comments

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ app = flask.Flask(__name__)
 
 CORS(app)
 
+# If cross origin is not used, the api wont be accessible by javascript
+
 @app.route("/api")
 # @cross_origin()  # this is optional if only want to make one function accessible
 def resp():
