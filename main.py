9f1b9d6f0f471827aa2d4cda3127d950827e7c71
Anchit Shrivastava <anchit.s@tatrasdata.com>
2021-12-15 13:34:50 +0530

    cross origin working

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import flask
 import json
-from flask_cors import CORS
+from flask_cors import CORS , cross_origin
 
 app = flask.Flask(__name__)
 
 CORS(app)
 
 @app.route("/api")
+# @cross_origin()  # this is optional if only want to make one function accessible
 def resp():
     return json.dumps({
         'test': 'testing the script'
